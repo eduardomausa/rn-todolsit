4b8f3abf288a2520f4b65a16c975cd60d4fa449b
Eduardo Mausa <mausaeduardo@gmail.com>
2022-09-20 17:37:19 -0300

    First RN challenge!

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,43 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from '../components/Header'
+import { Task, TasksList } from '../components/TasksList'
+import { TodoInput } from '../components/TodoInput'
 
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([])
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    if (!newTaskTitle) return
+
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    const taskAlreadyExists = updatedTasks.find(task => task.title === newTaskTitle)
+
+    if (taskAlreadyExists) return
+
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks([...tasks, data])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    const taskToToggle = updatedTasks.find(taskToToggle => taskToToggle.id === id)
+    if (!taskToToggle) return
+
+    taskToToggle.done = !taskToToggle.done
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks => tasks.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
@@ -26,10 +46,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
